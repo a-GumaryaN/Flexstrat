8a2abc4ac991dfd97a2f225911475d1d26f22d1e
-m <alireza313gomarian@gmail.com>
2025-04-15 10:33:36 +0330

    move indicator management flow from basement to indicator manager

--- FlexStrat/main.py
+++ FlexStrat/main.py
@@ -6,8 +6,9 @@ from threading import Thread
 from .API import *
 from .Visualizer import *
 from .Indicator import *
+from .Data import Shared_data
 
-trades = pd.DataFrame(columns=["open_time","open_price","status","type","close_time","SL","TP","size_lot"])
+trades = pd.DataFrame(columns=["ticket","open_time","open_price","status","type","close_time","SL","TP","size_lot"])
 
 class Basement:
 
@@ -24,11 +25,10 @@ class Basement:
     indicators_tree = []
 
     def __init__(self):
-        # initialize ioc manager
         self.ioc_manager = IOC_manager()
+        self.indicator_manager = Indicator_manager()
         self.ioc_manager.register(API,"csv_api",CSV)
-        self.ioc_manager.register(VISUALIZER,"mpl",MPL_visualizer)
-        self.ioc_manager.register(VISUALIZER,"CDD",CDD)
+        self.ioc_manager.register(VISUALIZER,"mpl_finance",MPL_visualizer)
 
     def run_indicators(self):
         pass
@@ -48,19 +48,13 @@ class Basement:
     def initialize(self,config):
         # set system configuration
         self.config = config
-        # initialize shared data
-        self.shared_data={
-            "candles":pd.DataFrame(columns=["open","close","high","low","volume"]),
-            "indicators":[],
-            "objects":[]
-        }
         #   initialize dependencies
         #       initialize api interface
         if API not in self.config:
             raise Exception("NO CONFIGURATION FOUND FOR API")
         api_config = self.config[API]
         self.api_interface = self.ioc_manager.contain(API,api_config["name"])
-        self.api_interface.configure(self.shared_data,api_config)
+        self.api_interface.configure(api_config)
 
         #       initialize visualizer
         if VISUALIZER not in self.config: 
@@ -68,21 +62,18 @@ class Basement:
         else :
             visualizer_config = self.config[VISUALIZER]
             self.visualizer = self.ioc_manager.contain(VISUALIZER,visualizer_config["name"])
-            self.visualizer.configure(self.shared_data,visualizer_config)
+            self.visualizer.configure(visualizer_config)
 
         #      initialize indicators
         if INDICATORS in self.config:
-            for indicator_config in self.config[INDICATORS]:
-                indicator = self.ioc_manager.contain(INDICATORS,indicator_config["type"])
-                indicator.configure(self.shared_data,indicator_config)
-                self.indicators.append(indicator)
+            self.indicator_manager.construct_indicators(self.config[INDICATORS])
 
         if CORE not in self.config:
             print(colored('WARNING : NO CONFIGURATION FOUND FOR CORE, SYSTEM WILL WORK IN TEST MODE', 'red'))
         else :
             core_config = self.config[CORE]
             self.core = self.ioc_manager.contain(CORE,core_config["name"])
-            self.core.configure(self.shared_data,core_config)
+            self.core.configure(core_config)
         
         return "system initialized successfully"
 
@@ -90,11 +81,6 @@ class Basement:
     def run(self):
         # creating threads
         self.api_thread = Thread(self.api_interface.run,)
-
-        if self.visualizer :
-            self.visualizer_thread = Thread(self.api_interface.run)
-
-        self.event_handler_thread = Thread(self.event_handler)
 
         
              
