8069c81bea6110179880db438245f7a1ce9314dc
a-GumaryaN <alireza313gomarian@gmail.com>
2025-04-20 23:49:20 +0330

    adding config and example

---
+++ example.py
@@ -0,0 +1,11 @@
+from FlexStrat import Basement
+from FlexStrat.API import API_repository
+from FlexStrat.Indicator import Indicator_repository
+from FlexStrat.Core import Core_repository
+from FlexStrat.Trade_manager import Trade_manager_repository
+from FlexStrat.Visualizer import Visualizer_repository
+
+
+basement = Basement()
+
+basement.show_repositories()
